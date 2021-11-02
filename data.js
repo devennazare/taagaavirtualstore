3549c5e6486325ec0afb4aef12a71466c770654d
Deven@12345 <deven@ottoedge.com>
2021-11-02 18:23:53 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,204 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-16",
+      "name": "16",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.012058902263897053,
+          "pitch": 0.19806307571076331,
+          "rotation": 0,
+          "target": "1-17"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-17",
+      "name": "17",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3982755769463235,
+          "pitch": 0.14101718888730197,
+          "rotation": 0,
+          "target": "2-18"
+        },
+        {
+          "yaw": -1.3893648652125936,
+          "pitch": 0.09653046673190957,
+          "rotation": 0,
+          "target": "3-19"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7189732390207428,
+          "pitch": -0.17887355338078592,
+          "title": "INNERS",
+          "text": "Adding a little more to the elegance of your outfit, we have slips and inners that can be worn inside and won’t take away the look of your dress in case you decide to doll up in a georgette attire"
+        },
+        {
+          "yaw": -0.37296723063164094,
+          "pitch": -0.22599762023233083,
+          "title": "BOTTOMS",
+          "text": "Our collection of pants will look appealing and feel elegant to wear at the same time. We offer from a mix of Salwar, Churidar, Trouser and Plazzo."
+        },
+        {
+          "yaw": 0.367358007480199,
+          "pitch": -0.24673715688845554,
+          "title": "Masks",
+          "text": "We are ready for the new normal. Introducing to you face masks in different colors of cotton and silk fabric to suit your comfort and at the same time something that will match your outfit."
+        },
+        {
+          "yaw": 0.7181621706229464,
+          "pitch": -0.21894721196732547,
+          "title": "Dupatta",
+          "text": "When talking about elegant designer wear, you can’t miss out on the dupatta. Every piece of dupatta stitched is done with detailing to give it the flawless look it deserves for your outfit."
+        }
+      ]
+    },
+    {
+      "id": "2-18",
+      "name": "18",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.114502009899617,
+          "pitch": 0.2098882244284077,
+          "rotation": 0,
+          "target": "1-17"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.18393462703550867,
+          "pitch": -0.40247034430458584,
+          "title": "FULL SUIT",
+          "text": "<font color=\"#000000\" face=\"Montserrat-SemiBold\"><span style=\"letter-spacing: 1px;\">Got that desi feeling? We have it all! Presenting to you classic and beautiful suits that blends in just well for any occasion.</span></font><br><div><br></div>"
+        },
+        {
+          "yaw": 0.20002567261637338,
+          "pitch": -0.4059261356436714,
+          "title": "ANARKALI",
+          "text": "Ethnic dresses are truly dresses with a beautiful soul. You won't have to compromise on the design you like. We have a variety of semi-stitched anarkalis to give you the perfect fit and look of your choice."
+        }
+      ]
+    },
+    {
+      "id": "3-19",
+      "name": "19",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9740558061986935,
+          "pitch": 0.2619107148323483,
+          "rotation": 0,
+          "target": "1-17"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24817063153655994,
+          "pitch": -0.3894554347160497,
+          "title": "KURTA",
+          "text": "When talking about elegant designer wear, you can’t miss out on the dupatta. Every piece of dupatta stitched is done with detailing to give it the flawless look it deserves for your outfit."
+        },
+        {
+          "yaw": 0.26915311819594834,
+          "pitch": -0.376861545278528,
+          "title": "SAREE",
+          "text": "Six yards of pure grace! Giving you a taste of georgette, chanderi and silk sarees with the touch of chikankari work done on them. A saree is always a good choice because it doesn’t need you to fit in but it makes you stand out."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
